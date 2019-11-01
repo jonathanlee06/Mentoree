25de5598d944c4797284830395c469bcfe59d0bd
junwei99 <45056907+junwei99@users.noreply.github.com>
2019-11-02 00:04:19 +0800

    not important

--- database/constants.php
+++ database/constants.php
@@ -6,5 +6,5 @@ define("USER","root");
 define("PASS","");
 define("DB","mentoree");
 
-define("DOMAIN","http://localhost/Mentoree/home.html");
+define("DOMAIN","http://localhost/Mentoree/index.html");
 ?>
