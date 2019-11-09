fc33841a52cd6a539a95b78f7bbaffcce797d1b4
jonathanlee06 <43515846+jonathanlee06@users.noreply.github.com>
2019-11-09 11:10:38 +0800

    Working pagination on home.php

--- home.php
+++ home.php
@@ -264,217 +264,94 @@
 				<div class="container">
 					<div class="row justify-content-center d-flex">
 						<div class="col-lg-8 post-list">
-							<ul class="cat-list">
+							<!-- <ul class="cat-list">
 								<li><a href="#">Recent</a></li>
 								<li><a href="#">Full Time</a></li>
 								<li><a href="#">Intern</a></li>
 								<li><a href="#">part Time</a></li>
-							</ul>
-							<div class="single-post d-flex flex-row">
-								<div class="thumb">
-									<img src="img/post.png" alt="">
-									<ul class="tags">
-										<li>
-											<a href="#">Art</a>
-										</li>
-										<li>
-											<a href="#">Media</a>
-										</li>
-										<li>
-											<a href="#">Design</a>					
-										</li>
+							</ul> -->
+							<?php
+								$conn = mysqli_connect("localhost", "root", "", "pagination");
+
+								$rows_per_page = 10;
+
+								$page = $_GET['page'];
+								if($page == "" || $page == "1"){
+									$page1 = 0;
+								}
+								else {
+									$page1 = ($page*10)-10;
+								}
+
+								$query = "SELECT * FROM listing LIMIT ".$page1. ", " . $rows_per_page;
+								$query_row = "SELECT * FROM listing";
+								$row_num = mysqli_query($conn, $query_row);
+								$count = mysqli_num_rows($row_num);
+								$num_of_rows = ceil($count/10);
+
+								$run  = mysqli_query($conn, $query);
+
+								while($rs = mysqli_fetch_array($run)) {
+									?>
+									<div class="single-post d-flex flex-row">
+										<div class="thumb">
+											<img src="img/post.png" alt="">
+											<ul class="tags">
+												<li>
+													<a href="#">Art</a>
+												</li>
+												<li>
+													<a href="#">Media</a>
+												</li>
+												<li>
+													<a href="#">Design</a>					
+												</li>
+											</ul>
+										</div>
+										<div class="details">
+											<div class="title d-flex flex-row justify-content-between">
+												<div class="titles">
+													<a href="single.html"><h4><?php echo $rs['first_name'].' '.$rs['last_name'] ?></h4></a>
+													<h6><?php echo $rs['email'] ?></h6>					
+												</div>
+												<ul class="btns">
+													<!-- <li><a href="#"><span class="lnr lnr-heart"></span></a></li>
+													<li><a href="#">Apply</a></li> -->
+												</ul>
+											</div>
+											<p>
+												<?php echo $rs['gender'] ?>
+											</p>
+											<!-- <h5>Job Nature: Full time</h5>
+											<p class="address"><span class="lnr lnr-map"></span> 56/8, Panthapath Dhanmondi Dhaka</p>
+											<p class="address"><span class="lnr lnr-database"></span> 15k - 25k</p> -->
+										</div>
+									</div>
+									<?php
+									// echo $rs['id'].' -> '.$rs['first_name'].'<br>';
+								}
+
+								?>
+								<div class="container">
+									<ul class="pagination justify-content-center">
+										<li class="disabled"><a href="">&laquo;</a></li>
+										<?php
+											for($i = 1; $i <= $num_of_rows; $i++){
+												echo "<li ";
+												if ($_GET['page'] == $i) {
+													echo ' class="active"';
+												}
+												echo "><a href='?page=$i'>$i</a></li>";
+												
+											}
+										?>
+										<li><a href="#">&raquo;</a></li>
 									</ul>
 								</div>
-								<div class="details">
-									<div class="title d-flex flex-row justify-content-between">
-										<div class="titles">
-											<a href="single.html"><h4>Creative Art Designer</h4></a>
-											<h6>Premium Labels Limited</h6>					
-										</div>
-										<ul class="btns">
-											<li><a href="#"><span class="lnr lnr-heart"></span></a></li>
-											<li><a href="#">Apply</a></li>
-										</ul>
-									</div>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-									</p>
-									<h5>Job Nature: Full time</h5>
-									<p class="address"><span class="lnr lnr-map"></span> 56/8, Panthapath Dhanmondi Dhaka</p>
-									<p class="address"><span class="lnr lnr-database"></span> 15k - 25k</p>
-								</div>
-							</div>
-							<div class="single-post d-flex flex-row">
-								<div class="thumb">
-									<img src="img/post.png" alt="">
-									<ul class="tags">
-										<li>
-											<a href="#">Art</a>
-										</li>
-										<li>
-											<a href="#">Media</a>
-										</li>
-										<li>
-											<a href="#">Design</a>					
-										</li>
-									</ul>
-								</div>
-								<div class="details">
-									<div class="title d-flex flex-row justify-content-between">
-										<div class="titles">
-											<a href="single.html"><h4>Creative Art Designer</h4></a>
-											<h6>Premium Labels Limited</h6>					
-										</div>
-										<ul class="btns">
-											<li><a href="#"><span class="lnr lnr-heart"></span></a></li>
-											<li><a href="#">Apply</a></li>
-										</ul>
-									</div>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-									</p>
-									<h5>Job Nature: Full time</h5>
-									<p class="address"><span class="lnr lnr-map"></span> 56/8, Panthapath Dhanmondi Dhaka</p>
-									<p class="address"><span class="lnr lnr-database"></span> 15k - 25k</p>
-								</div>
-							</div>
-							<div class="single-post d-flex flex-row">
-								<div class="thumb">
-									<img src="img/post.png" alt="">
-									<ul class="tags">
-										<li>
-											<a href="#">Art</a>
-										</li>
-										<li>
-											<a href="#">Media</a>
-										</li>
-										<li>
-											<a href="#">Design</a>					
-										</li>
-									</ul>
-								</div>
-								<div class="details">
-									<div class="title d-flex flex-row justify-content-between">
-										<div class="titles">
-											<a href="single.html"><h4>Creative Art Designer</h4></a>
-											<h6>Premium Labels Limited</h6>					
-										</div>
-										<ul class="btns">
-											<li><a href="#"><span class="lnr lnr-heart"></span></a></li>
-											<li><a href="#">Apply</a></li>
-										</ul>
-									</div>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-									</p>
-									<h5>Job Nature: Full time</h5>
-									<p class="address"><span class="lnr lnr-map"></span> 56/8, Panthapath Dhanmondi Dhaka</p>
-									<p class="address"><span class="lnr lnr-database"></span> 15k - 25k</p>
-								</div>
-							</div>		
-							<div class="single-post d-flex flex-row">
-								<div class="thumb">
-									<img src="img/post.png" alt="">
-									<ul class="tags">
-										<li>
-											<a href="#">Art</a>
-										</li>
-										<li>
-											<a href="#">Media</a>
-										</li>
-										<li>
-											<a href="#">Design</a>					
-										</li>
-									</ul>
-								</div>
-								<div class="details">
-									<div class="title d-flex flex-row justify-content-between">
-										<div class="titles">
-											<a href="single.html"><h4>Creative Art Designer</h4></a>
-											<h6>Premium Labels Limited</h6>					
-										</div>
-										<ul class="btns">
-											<li><a href="#"><span class="lnr lnr-heart"></span></a></li>
-											<li><a href="#">Apply</a></li>
-										</ul>
-									</div>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-									</p>
-									<h5>Job Nature: Full time</h5>
-									<p class="address"><span class="lnr lnr-map"></span> 56/8, Panthapath Dhanmondi Dhaka</p>
-									<p class="address"><span class="lnr lnr-database"></span> 15k - 25k</p>
-								</div>
-							</div>
-							<div class="single-post d-flex flex-row">
-								<div class="thumb">
-									<img src="img/post.png" alt="">
-									<ul class="tags">
-										<li>
-											<a href="#">Art</a>
-										</li>
-										<li>
-											<a href="#">Media</a>
-										</li>
-										<li>
-											<a href="#">Design</a>					
-										</li>
-									</ul>
-								</div>
-								<div class="details">
-									<div class="title d-flex flex-row justify-content-between">
-										<div class="titles">
-											<a href="single.html"><h4>Creative Art Designer</h4></a>
-											<h6>Premium Labels Limited</h6>					
-										</div>
-										<ul class="btns">
-											<li><a href="#"><span class="lnr lnr-heart"></span></a></li>
-											<li><a href="#">Apply</a></li>
-										</ul>
-									</div>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-									</p>
-									<h5>Job Nature: Full time</h5>
-									<p class="address"><span class="lnr lnr-map"></span> 56/8, Panthapath Dhanmondi Dhaka</p>
-									<p class="address"><span class="lnr lnr-database"></span> 15k - 25k</p>
-								</div>
-							</div>
-							<div class="single-post d-flex flex-row">
-								<div class="thumb">
-									<img src="img/post.png" alt="">
-									<ul class="tags">
-										<li>
-											<a href="#">Art</a>
-										</li>
-										<li>
-											<a href="#">Media</a>
-										</li>
-										<li>
-											<a href="#">Design</a>					
-										</li>
-									</ul>
-								</div>
-								<div class="details">
-									<div class="title d-flex flex-row justify-content-between">
-										<div class="titles">
-											<a href="single.html"><h4>Creative Art Designer</h4></a>
-											<h6>Premium Labels Limited</h6>					
-										</div>
-										<ul class="btns">
-											<li><a href="#"><span class="lnr lnr-heart"></span></a></li>
-											<li><a href="#">Apply</a></li>
-										</ul>
-									</div>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut dolore magna aliqua.
-									</p>
-									<h5>Job Nature: Full time</h5>
-									<p class="address"><span class="lnr lnr-map"></span> 56/8, Panthapath Dhanmondi Dhaka</p>
-									<p class="address"><span class="lnr lnr-database"></span> 15k - 25k</p>
-								</div>
-							</div>															
-							<div class="single-post d-flex flex-row">
+								<?php
+							?>
+
+							<!-- <div class="single-post d-flex flex-row">
 								<div class="thumb">
 									<img src="img/post.png" alt="">
 									<ul class="tags">
@@ -510,6 +387,22 @@
 							</div>	
 							
 							<a class="text-uppercase loadmore-btn mx-auto d-block" href="category.html">Load More job Posts</a>
+							<div class="container">
+								<ul class="pagination justify-content-center">
+									<li class="disabled"><a href="">&laquo;</a></li>
+									<li class="active"><a href="">1</a></li>
+									<li><a href="">2</a></li>
+									<li><a href="">3</a></li>
+									<li><a href="">4</a></li>
+									<li><a href="">5</a></li>
+									<li><a href="">6</a></li>
+									<li><a href="">7</a></li>
+									<li><a href="">8</a></li>
+									<li><a href="">9</a></li>
+									<li><a href="">10</a></li>
+									<li><a href="#">&raquo;</a></li>
+								</ul>
+							</div> -->
 
 						</div>
 						<div class="col-lg-4 sidebar">
